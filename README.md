8521a1010a2a5721bc2ee6b242ab19775a1f13aa
alpha-tango-kilo <git@heyatk.com>
2021-08-20 13:10:51 +0100

    Start lazy OOP approach

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ It aims to reproduce the important core functionality in a minimal and easily au
 
 * [Parcel](https://parceljs.org/) - for building and development
 * [TypeScript](https://www.typescriptlang.org/) - safer and more self-explanatory code
+* [WebExtension Polyfill](https://github.com/Lusito/webextension-polyfill-ts) - to be able to use Mozilla's `Promise`-based API in Chrome*
+
+\* `Promise` APIs for `chrome` are only available with [version 3 manifests](https://developer.chrome.com/docs/extensions/mv3/intro/mv3-overview/#feature-summary), which are incompatible with Parcel at this time ([Parcel tracking issue](https://github.com/parcel-bundler/parcel/issues/6079)).
+In any case, polyfill improves cross-browser compatibility
 
 ## Permissions explanation
 
--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,55 @@
-console.log("Hello world!");
+import { browser } from "webextension-polyfill-ts";
+
+// TODO: account for incompatible subdomains
+const ZENDESK_URLS = ["http://*.zendesk.com/*", "https://*.zendesk.com/*"];
+
+class MasterTab {
+    private id: number | null
+
+    constructor() {
+        this.id = null;
+    }
+
+    /**
+     * Get current master tab ID
+     * Checked to make sure it is valid
+     * If the current ID is null or no longer valid, tries to get a new one using findMasterTab()
+     */
+    async getID(): Promise<number> {
+        if (this.id !== null) {
+            this.id = await browser.tabs.get(this.id)
+                .then(tab => tab.id!)
+                .catch(_ => this.findMasterTab());
+        } else {
+            console.log("Searching for Zendesk tab to make master");
+            this.id = await this.findMasterTab();
+        }
+        return this.id;
+    }
+
+    /**
+     * Get ID of first Zendesk tab if it exists
+     */
+    private async findMasterTab(): Promise<number | null> {
+        let tabs = await browser.tabs.query({ url: ZENDESK_URLS });
+        if (tabs.length == 0) {
+            console.error("No Zendesk tab found to consider master");
+            return null;
+        } else {
+            return tabs[0].id!;
+        }
+    }
+}
+
+let masterTab = new MasterTab();
+
+browser.tabs.onUpdated.addListener(async (id, _) => {
+    let masterID = await masterTab.getID();
+    if (id === masterID) {
+        return;
+    }
+    
+    // Make ZD master tab active
+    browser.tabs.update(masterID, { active: true, highlighted: true });
+    // TODO: merge
+}, { urls: ZENDESK_URLS });
